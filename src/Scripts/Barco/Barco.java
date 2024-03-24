5005e81f4455bcc2f23261b69902d345cb1eeb17
João Augusto Tolentino <sjats2021@gmail.com>
2024-03-24 17:50:39 -0300

    Criado classe do barco

--- src/Scripts/Barco/Barco.java
+++ src/Scripts/Barco/Barco.java
@@ -13,5 +13,72 @@ public class Barco {
 	private Item[] carga;
 	private int limiteCarga;
 	
-	
+	private float durabilidadeAtual;
+	private float durabilidadeMax;
+
+	/*
+	 * CONSTRUTOR
+	 */
+	public Barco(String tipo, int velocidade, int limiteCarga, float durabilidadeMax)
+	{
+		this.tipo = tipo;
+		this.velocidade = velocidade;
+		
+		this.carga = new Item[limiteCarga];
+		this.limiteCarga = limiteCarga;
+
+		this.durabilidadeAtual = durabilidadeMax;
+		this.durabilidadeMax = durabilidadeMax;
+	}
+
+	/*
+	 * GETTERS E SETTERS
+	 */
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public int getVelocidade() {
+		return velocidade;
+	}
+
+	public void setVelocidade(int velocidade) {
+		this.velocidade = velocidade;
+	}
+
+	public Item[] getCarga() {
+		return carga;
+	}
+
+	public void setCarga(Item[] carga) {
+		this.carga = carga;
+	}
+
+	public int getLimiteCarga() {
+		return limiteCarga;
+	}
+
+	public void setLimiteCarga(int limiteCarga) {
+		this.limiteCarga = limiteCarga;
+	}
+
+	public float getDurabilidadeAtual() {
+		return durabilidadeAtual;
+	}
+
+	public void setDurabilidadeAtual(float durabilidadeAtual) {
+		this.durabilidadeAtual = durabilidadeAtual;
+	}
+
+	public float getDurabilidadeMax() {
+		return durabilidadeMax;
+	}
+
+	public void setDurabilidadeMax(float durabilidadeMax) {
+		this.durabilidadeMax = durabilidadeMax;
+	}	
 }
