1fd3f8f087ad11ac7426acbb3b1d04bfe8f35236
Pigas22 <tholzc@gmail.com>
2024-07-04 21:40:01 -0300

    refactor: adicionado conteúdo à Classe Main

---
+++ src/Scripts/Main.java
@@ -0,0 +1,50 @@
+package Scripts;
+
+import java.io.IOException;
+
+import Scripts.Ataque.*;
+import Scripts.Atributo.*;
+import Scripts.Interfaces.*;
+import Scripts.ListaDupla.*;
+import Scripts.Pessoa.*;
+
+// outro teste
+public class Main {
+    public static void main(String [] args) throws IOException, InterruptedException {
+        Interfaces teste = new Interfaces();
+        
+        boolean continuar = true;
+
+        do {
+            int foda = teste.menu();
+
+            switch (foda)
+            {
+                case 0:
+
+                    // Finalizar Scanners
+                    teste.Finalizar();
+
+                    // Finalizar continuação do programa
+                    continuar = false;
+
+                    break;
+                
+                case 2:
+
+                    break;
+
+                case 3:
+                    
+                    // Mostrar créditos
+                    teste.creditos();
+
+                    break;
+            }
+
+            // Limpar tela do terminal
+            teste.limparTerminal();
+
+        } while (continuar);
+    }
+}
